660465016a7e0d8ae636a37f2329626f1bcad10e
jctanakasaraiva <59887192+jctanakasaraiva@users.noreply.github.com>
2023-12-19 20:39:39 -0400

    Deleted unused fields from Parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -8,23 +8,13 @@ public class Parallax : MonoBehaviour
     [SerializeField] private GameObject waves;
     [SerializeField] private float parallaxSpeed;
     private float length;
-    private float startPosition; //TODO: Remove
     private float actualPosition;
-    // Start is called before the first frame update
 
     private bool instantiateWave = false;
 
     private void Start()
     {
-        startPosition = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void FixedUpdate()
@@ -35,7 +25,6 @@ public class Parallax : MonoBehaviour
     private void MoveParallax()
     {
         actualPosition = transform.position.x;
-
         if (instantiateWave == false)
         {
             if (actualPosition <= 0)
@@ -45,12 +34,9 @@ public class Parallax : MonoBehaviour
                 temporaryWave.transform.position = new Vector3(transform.position.x + length - 0.3f, transform.position.y, 0);
             }
         }
-
         if(actualPosition <= length * -1){
             Destroy(this.gameObject);
         }
-
-
         transform.position = new Vector3(actualPosition + parallaxSpeed * Time.deltaTime, transform.position.y, transform.position.z);
     }
 }
